275702d6b3f0e2a0fdb1eea8d78edcbda368ec08
AKIHIRO <heeero75@gmail.com>
2020-02-26 00:01:29 +0000

    first commit

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -14,7 +14,7 @@ class ProfileController extends Controller
     
     public function create()
     {
-        return redirect('admin/profile/create');
+        return redirect('admin.profile.create');
     }
     
     public function edit()
@@ -24,6 +24,7 @@ class ProfileController extends Controller
     
     public function update()
     {
-        return redirect('admin/profile/profile/edit');
+        return redirect('admin.profile.profile.edit');
     }
 }
+
